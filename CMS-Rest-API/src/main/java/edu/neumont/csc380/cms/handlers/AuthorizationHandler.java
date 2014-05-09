9fdbf08eb369c7afdd4ac8744944300b0f2c9f1b
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-05-08 18:55:12 -0600

    fixed CMS auth

--- CMS-Rest-API/src/main/java/edu/neumont/csc380/cms/handlers/AuthorizationHandler.java
+++ CMS-Rest-API/src/main/java/edu/neumont/csc380/cms/handlers/AuthorizationHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
 import org.apache.cxf.jaxrs.ext.RequestHandler;
@@ -17,24 +18,25 @@ public class AuthorizationHandler implements RequestHandler {
 		String method = (String) m.get(Message.HTTP_REQUEST_METHOD);
 		if (method.equals("POST") || method.equals("PUT")
 				|| method.equals("DELETE")) {
-			return authorize(getToken(m));
+			String token = getToken(m);
+			if (token == null) {
+				return Response.status(Status.UNAUTHORIZED).build();
+			} else {
+				// AuthClientImpl auth = new AuthClientImpl();
+				// auth.
+			}
 		}
 		return null;
 	}
 
 	private String getToken(Message m) {
-		return ((ArrayList<String>) ((Map<String, ArrayList<String>>) m
-				.get(Message.PROTOCOL_HEADERS)).get("Authorization")).get(0);
-	}
-
-	private Response authorize(String token) {
-		if (token != null) {
-			// TODO: somehow verify validity
-		} else {
-			// TODO: return forbidden or tell user to authorize?
-
+		try {
+			return ((ArrayList<String>) ((Map<String, ArrayList<String>>) m
+					.get(Message.PROTOCOL_HEADERS)).get("Authorization"))
+					.get(0);
+		} catch (NullPointerException e) {
+			return null;
 		}
-		return null;
 	}
 
 }
